f67bd5c43fc4a7423b8d2d8a57f2702cb234f595
Steve Gillbank <sgillbank@users.noreply.github.com>
2017-09-25 17:27:39 -0700

    Update Refund.cs

--- Samples/net40/Transaction/Refund.cs
+++ Samples/net40/Transaction/Refund.cs
@@ -58,7 +58,7 @@ namespace Samples.Net40
 
                 // POST
                 byte[] data = System.Text.Encoding.UTF8.GetBytes(datastring.ToString());
-                var url = "https://sandbox.payfabric.com/V3/PayFabric/rest/api/transaction/process";
+                var url = "https://sandbox.payfabric.com/Payment/API/transaction/process";
                 HttpWebRequest httpWebRequest = WebRequest.Create(url) as HttpWebRequest;
                 httpWebRequest = WebRequest.Create(url) as HttpWebRequest;
                 httpWebRequest.ContentType = "application/json; charset=utf-8";
@@ -148,7 +148,7 @@ namespace Samples.Net40
 
                 // POST
                 byte[] data = System.Text.Encoding.UTF8.GetBytes(datastring.ToString());
-                var url = "https://sandbox.payfabric.com/V3/PayFabric/rest/api/transaction/process";
+                var url = "https://sandbox.payfabric.com/Payment/API/transaction/process";
                 HttpWebRequest httpWebRequest = WebRequest.Create(url) as HttpWebRequest;
                 httpWebRequest = WebRequest.Create(url) as HttpWebRequest;
                 httpWebRequest.ContentType = "application/json; charset=utf-8";
