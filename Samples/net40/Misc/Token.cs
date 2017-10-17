2638c633fec2162112974b2584a56a8d96a83842
Steve Gillbank <sgillbank@users.noreply.github.com>
2017-10-17 13:35:22 -0700

    Update Token.cs

--- Samples/net40/Misc/Token.cs
+++ Samples/net40/Misc/Token.cs
@@ -27,7 +27,7 @@ namespace Samples.Net40
             {
 
                 // Replace url when going live
-                var url = "https://sandbox.payfabric.com/V3/PayFabric/rest/api/token/create";
+                var url = "https://sandbox.payfabric.com/payment/api/token/create";
 
                 HttpWebRequest httpWebRequest = WebRequest.Create(url) as HttpWebRequest;
                 httpWebRequest.ContentType = "application/json; charset=utf-8";
@@ -37,6 +37,7 @@ namespace Samples.Net40
                 httpWebRequest.Headers["authorization"] = "0ad64468-f4bc-0c99-4e31-bd08dd862c43|123456abc";
 
                 HttpWebResponse httpWebResponse = httpWebRequest.GetResponse() as HttpWebResponse;
+                ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
                 Stream responseStream = httpWebResponse.GetResponseStream();
                 StreamReader streamReader = new StreamReader(responseStream);
                 string result = streamReader.ReadToEnd();
