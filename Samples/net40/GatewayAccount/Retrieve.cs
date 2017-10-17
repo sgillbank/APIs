3ec9b7bfee8c4a37808422fc8272ebe7a22c9026
Steve Gillbank <sgillbank@users.noreply.github.com>
2017-10-17 13:31:58 -0700

    Update Retrieve.cs

--- Samples/net40/GatewayAccount/Retrieve.cs
+++ Samples/net40/GatewayAccount/Retrieve.cs
@@ -31,12 +31,13 @@ namespace Samples.Net40
         {
             try
             {
-                var url = "https://sandbox.payfabric.com/V3/PayFabric/rest/api/setupid/" + gatewayAccountId.ToString();
+                var url = "https://sandbox.payfabric.com/payment/api/setupid/" + gatewayAccountId.ToString();
                 HttpWebRequest httpWebRequest = WebRequest.Create(url) as HttpWebRequest;
                 httpWebRequest.Method = "GET";
                 httpWebRequest.ContentType = "application/json; charset=utf-8";
                 httpWebRequest.Headers["authorization"] = new Token().Create();
                 HttpWebResponse httpWebResponse = httpWebRequest.GetResponse() as HttpWebResponse;
+                ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
                 Stream responseStream = httpWebResponse.GetResponseStream();
                 StreamReader streamReader = new StreamReader(responseStream);
                 string result = streamReader.ReadToEnd();   // JSON result
@@ -50,7 +51,7 @@ namespace Samples.Net40
                 // Sample response
                 // ------------------------------------------------------
                 // Response text is a gateway account object with json format
-                // Go to https://github.com/PayFabric/APIs/wiki/API-Objects#gateway-account for more details about gateway account object.
+                // Go to https://github.com/PayFabric/APIs/blob/master/Sections/PaymentGatewayProfiles.md#payment-gateway-profiles for more details about gateway account object.
                 // ------------------------------------------------------
 
             }
