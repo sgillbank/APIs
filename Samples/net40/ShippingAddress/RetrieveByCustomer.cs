390055cf530969efbf3b867cd506ef1034dc6830
Steve Gillbank <sgillbank@users.noreply.github.com>
2017-10-17 13:38:48 -0700

    Update RetrieveByCustomer.cs

--- Samples/net40/ShippingAddress/RetrieveByCustomer.cs
+++ Samples/net40/ShippingAddress/RetrieveByCustomer.cs
@@ -28,12 +28,13 @@ namespace Samples.Net40
         {
             try
             {
-                var url = "https://sandbox.payfabric.com/V3/PayFabric/rest/api/addresses" + "/" + customer;
+                var url = "https://sandbox.payfabric.com/payment/api/addresses" + "/" + customer;
                 HttpWebRequest httpWebRequest = WebRequest.Create(url) as HttpWebRequest;
                 httpWebRequest.Method = "GET";
                 httpWebRequest.ContentType = "application/json; charset=utf-8";
                 httpWebRequest.Headers["authorization"] = new Token().Create();
                 HttpWebResponse httpWebResponse = httpWebRequest.GetResponse() as HttpWebResponse;
+                ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
                 Stream responseStream = httpWebResponse.GetResponseStream();
                 StreamReader streamReader = new StreamReader(responseStream);
                 string result = streamReader.ReadToEnd();   // JSON result
@@ -47,7 +48,7 @@ namespace Samples.Net40
                 // Sample response
                 // ------------------------------------------------------
                 // Response text is an array of address object with json format
-                // Go to https://github.com/PayFabric/APIs/wiki/API-Objects#address for more details about address object.
+                // Go to https://github.com/PayFabric/APIs/blob/master/Sections/Addresses.md#addresses for more details about address object.
                 // ------------------------------------------------------
 
             }
