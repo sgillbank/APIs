353eac71ad5a814004af257df168ce12deee893c
Steve Gillbank <sgillbank@users.noreply.github.com>
2017-10-17 13:33:51 -0700

    Update RetrieveAll.cs

--- Samples/net40/GatewayAccount/RetrieveAll.cs
+++ Samples/net40/GatewayAccount/RetrieveAll.cs
@@ -27,12 +27,13 @@ namespace Samples.Net40
         {
             try
             {
-                var url = "https://sandbox.payfabric.com/V3/PayFabric/rest/api/setupid";
+                var url = "https://sandbox.payfabric.com/payment/api/setupid";
                 HttpWebRequest httpWebRequest = WebRequest.Create(url) as HttpWebRequest;
                 httpWebRequest.Method = "GET";
                 httpWebRequest.ContentType = "application/json; charset=utf-8";
                 httpWebRequest.Headers["authorization"] = new Token().Create();
                 HttpWebResponse httpWebResponse = httpWebRequest.GetResponse() as HttpWebResponse;
+                ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
                 Stream responseStream = httpWebResponse.GetResponseStream();
                 StreamReader streamReader = new StreamReader(responseStream);
                 string result = streamReader.ReadToEnd();   // JSON result
